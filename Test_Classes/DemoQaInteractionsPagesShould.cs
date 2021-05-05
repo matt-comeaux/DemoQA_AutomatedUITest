5ac199b1a72729985867b6a740f9cf85af532be9
Matthew Comeaux <77807412+matt-comeaux@users.noreply.github.com>
2021-05-04 20:38:42 -0500

    Initial Upload.
    This contains the framework for the automated UI tests for https://demoqa.com . This project was created by Matthew Comeaux (me).
    
    The following is included in this upload:
    1) POM for all pages that will be tested on website.
    2) Test classes along with initial load functions for main pages.
    
    Note: The packages will be added in a secondary upload do to file upload limits.

---
+++ Test_Classes/DemoQaInteractionsPagesShould.cs
@@ -0,0 +1,54 @@
+﻿/*
+Name: DemoQaInteractionsPagesShould.cs
+Purpose: Contains the test class for the interaction pages on www.demoqa.com. This includes testing for all interaction pages under the interactions section. 
+Author: Matthew Comeaux. Github: https://www.github.com/matt-comeaux Linkedin: https://www.linkedin.com/in/matthew-comeaux
+Created On: 5/3/2021
+First Uploaded To Github On: 5/4/2021
+
+MIT License
+
+Copyright (c) [2021] [Matthew H. Comeaux]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ */
+
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using TestAutomation_DemoQA.Page_Object_Models.Interactions;
+using Xunit;
+
+namespace TestAutomation_DemoQA
+{
+    public class DemoQaInteractionsPagesShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void Load()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                var interactionsPage = new Interactions_MainPage(driver);
+                interactionsPage.LoadPage();
+            }
+        }
+    }
+}
