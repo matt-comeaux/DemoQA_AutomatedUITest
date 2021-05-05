6b28bd513b706665bb30a6bcda40f297cdc36976
Matthew Comeaux <77807412+matt-comeaux@users.noreply.github.com>
2021-05-05 14:31:18 -0500

    Updated Home Page POM & Test Class
    Updated the home page POM and test class to fix typo.

---
+++ HomePage.cs
@@ -0,0 +1,63 @@
+﻿/*
+Name: HomePage.cs
+Purpose: Contains the Page Object Model for the home page on www.demoqa.com. 
+Author: Matthew Comeaux. Github: https://www.github.com/matt-comeaux Linkedin: https://www.linkedin.com/in/matthew-comeaux
+Created On: 5/3/2021
+First Uploaded To Github On: 5/4/2021
+
+MIT License
+
+Copyright (c) [2021] [Matthew H. Comeaux]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ */
+
+using System;
+using OpenQA.Selenium;
+
+namespace TestAutomation_DemoQA.Page_Object_Models.HomePage
+{
+    class HomePage
+    {
+        private readonly IWebDriver Driver;
+        private readonly string url = "https://demoqa.com/";
+        private readonly string pageTitle = "ToolsQA";
+
+        public HomePage(IWebDriver driver)
+        {
+            this.Driver = driver;
+        }
+        public void LoadPage()
+        {
+            Driver.Navigate().GoToUrl(url);
+            EnsurePageLoaded();
+        }
+
+        public void EnsurePageLoaded()
+        {
+            bool isLoaded = (Driver.Url == url) && (Driver.Title == pageTitle);
+
+            if (!isLoaded)
+            {
+                throw new Exception($"The requested page did not load correctly. The page url is: '{url}' The page source is: \r\n '{Driver.PageSource}'");
+            }
+        }
+    }
+}
